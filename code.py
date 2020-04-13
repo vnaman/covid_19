190608c9b1b7a1d9b7aebc8d44e402a89a52c23a
Naman <vnaman555%gmail.com>
2020-04-13 21:24:22 +0530

    removed the history feature and some changes

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-''' This is the code for the project '''
+''' This file prints some statistics for the selected country '''
 
 ''' importing all the modules required '''
 import requests
@@ -9,20 +9,16 @@ country = input('Enter the name of the country for which you want to check')
 
 ''' calling the api '''
 url_for_statistics = "https://covid-193.p.rapidapi.com/statistics"
-url_for_history = "https://covid-193.p.rapidapi.com/history"
 
 headers = {
     'x-rapidapi-host': "covid-193.p.rapidapi.com",
     'x-rapidapi-key': "e1608910c9msh3d6426308d4b3b9p15de94jsn7fc358704657"
     }
-querystring = {"day":"2020-04-06","country":country}
+
 
 '''creating the json of the data '''
 response_for_statistics = requests.get(url_for_statistics,headers=headers)
 country_dict = response_for_statistics.json()
-response_for_history = requests.get(url_for_history,headers=headers,params=querystring)
-history_dict = response_for_history.json()
-
 
 
 '''printing the country's statistics '''
@@ -33,12 +29,7 @@ for each in country_dict['response']:
         total_cases = each['cases']['total']
         total_deaths = each['deaths']['total']
         print(f'Name of the country : {name}')
-        print(f'Number of new cases : {new_cases}')
+        print(f'Number of new cases for today: {new_cases}')
         print(f'Total number of cases : {total_cases}')
         print(f'Total number of deaths : {total_deaths}')
 
-''' calculating day wise deaths'''
-end_of_day_deaths = history_dict['response'][0]['deaths']['total']
-start_of_day_deaths = history_dict['response'][-1]['deaths']['total']
-death_for_day = int(end_of_day_deaths) - int(start_of_day_deaths)
-print(f'death count for today is {death_for_day}')
